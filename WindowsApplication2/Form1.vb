1fb41de5915c18a8ac68216e5d59fc916c7da2db
suzuki040 <suzuki040@gmail.com>
2014-12-12 18:24:55 +0900

    Update Form1.vb

--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        MsgBox("Hello world 2")
+        MsgBox("Hello world 2 20141212_1825")
     End Sub
 End Class
