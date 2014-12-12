c77a2c9473fa23d4e4052554773d98e8ee2324e9
suzuki040 <suzuki040@gmail.com>
2014-12-12 18:34:37 +0900

    row add

--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -2,5 +2,6 @@
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         MsgBox("Hello world 2_20141212_1830")
+        MsgBox("Hello world 2_20141212_1830_2")
     End Sub
 End Class
