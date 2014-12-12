764772af6a87c033ca9b0c0cf5602e8090b7c246
suzuki <suzuki040@gmail.com>
2014-12-12 15:23:42 +0900

    1stcommit

---
+++ WindowsApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("WindowsApplication2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("株式会社システムプロ")> 
+<Assembly: AssemblyProduct("WindowsApplication2")> 
+<Assembly: AssemblyCopyright("Copyright © 株式会社システムプロ 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("f7d09a92-0991-42e2-8f56-33d2ca301740")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
