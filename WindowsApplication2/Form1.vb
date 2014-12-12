95ca7418f016a381fc5754d039be045bc65feb02
suzuki <suzuki040@gmail.com>
2014-12-12 18:03:56 +0900

    メッセージ文言変更
    変更したかったから

--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        MsgBox("Hello world")
+        MsgBox("Hello world 2")
     End Sub
 End Class
